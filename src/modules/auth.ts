dcc1a3f33f86cfbdb1e4ffa29e43e4c5cb2367a1
delanobmarques <delanobmarques@gmail.com>
2023-04-22 17:00:20 -0300

    implementing a middleware to protect the API routes. The middleware first checks to see if a bearer token is present. If not, it returns and 401 not authorized error

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -9,3 +9,33 @@ export const createJWT = (user) => {
     )
     return token
 }
+
+export const protect = (req, res, next) => {
+     //bearer token:generic way of describing someone having the ability to send up a token which can be any type of token (jwt, apikey, acces token,...)
+    const bearer = req.headers.authorization
+
+    if(!bearer) {
+        res.status(401)
+        res.json({message: 'not authorized'})
+        return
+    }
+
+    const [, token] = bearer.split(' ') //bearer kjbcdksbcjhbc(token)
+
+    if(!token) {
+        res.status(401)
+        res.json({message: 'not valid token'})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+    } catch (error) {
+        console.error(error)
+        res.status(401)
+        res.json({message: 'not valid token'})
+        return
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app = express()
 
+app.use(cors())
 //every request has to go through morgan - console.logs it and goes to the next thing on the stack
 app.use(morgan('dev'))
 app.use(express.json())//middleware to allows a client to send the server json 
@@ -21,7 +24,7 @@ app.get('/', (req, res) => {
     res.json({message: 'hello'})
 })
 
-//for everything that has '/api' I want the app to use the reouter
-app.use('/api', router)
+//for everything that has '/api' I want the app to pass through protect middleware and use the router
+app.use('/api', protect, router)
 
 export default app
