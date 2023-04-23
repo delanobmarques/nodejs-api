5f22d9f2b92b9a67a2399a9117594406cd054c8d
delanobmarques <delanobmarques@gmail.com>
2023-04-23 19:24:06 -0300

    create update handlers

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import {Router} from 'express'
 import { body, oneOf, validationResult } from "express-validator"
 import { createProduct, deleteProduct, getOneProduct, getProducts } from './handlers/product'
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update'
 import { handleInputErrors } from './modules/middleware'
-
 
 const router = Router()
 /*  put vs patch 
@@ -23,22 +23,21 @@ router.delete('/product/:id', deleteProduct)
 /**
  * Update routes
  * */ 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id', 
     body('title').optional(), 
     body('body').optional(), 
     body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
     body('version').optional(), 
-    () => {}
+    updateUpdate
 )
 router.post('/update', 
     body('title').exists().isString(),
     body('body').exists().isString(),
-    () => {}
+    createUpdate
 )
-
-router.delete('/update/:id', () => {})
+router.delete('/update/:id', deleteUpdate)
 
 /* * 
  * Update Point routes
